0c025d709a4aafeff0ddf0f2d48edc6365c6c435
Peter J. Kranz <peter@myref.net>
2012-12-11 01:42:47 +0100

    added default values to constants

--- mobile/android/src/main/java/org/inrain/pmap/Constants.java
+++ mobile/android/src/main/java/org/inrain/pmap/Constants.java
@@ -2,5 +2,8 @@ package org.inrain.pmap;
 
 public class Constants {
     public static final String PREFERENCE_USERNAME_KEY = "pref_username";
+    public static final String PREFERENCE_USERNAME_DEFAULT = "anonymous";
+
     public static final String PREFERENCE_SERVER_URL_KEY = "pref_server_url";
+    public static final String PREFERENCE_SERVER_URL_DEFAULT = "";
 }
