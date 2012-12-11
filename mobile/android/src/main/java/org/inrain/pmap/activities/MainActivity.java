0995353d52ccd21d41f4290e0cfa4d1af8655180
Peter J. Kranz <peter@myref.net>
2012-12-11 02:20:40 +0100

    started using logger + formatter

--- mobile/android/src/main/java/org/inrain/pmap/activities/MainActivity.java
+++ mobile/android/src/main/java/org/inrain/pmap/activities/MainActivity.java
@@ -6,12 +6,11 @@ import java.util.List;
 import java.util.Map;
 
 import org.inrain.pmap.R;
-import org.inrain.pmap.R.id;
-import org.inrain.pmap.R.menu;
+import org.inrain.pmap.Util;
 import org.inrain.pmap.services.LocationUpdateService;
+import org.slf4j.Logger;
 
 import android.app.ListActivity;
-import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -23,13 +22,16 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 public class MainActivity extends ListActivity {
+    private final Logger logger = Util.createLogger();
+
     private ListAdapter listAdapter;
-    
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        System.out.println("test");
+
+        logger.trace("onCreate");
+
         startService(new Intent(this, LocationUpdateService.class));
 
         List<Map<String, String>> list = new ArrayList<Map<String, String>>();
@@ -37,36 +39,36 @@ public class MainActivity extends ListActivity {
         map.put("line1", "locate your friends");
         map.put("line2", "show distances of your friends");
         list.add(map);
-        
-        listAdapter = new SimpleAdapter(this, list, android.R.layout.two_line_list_item, new String[] { "line1", "line2" },
-                new int[] { android.R.id.text1, android.R.id.text2 });
+
+        listAdapter = new SimpleAdapter(this, list, android.R.layout.two_line_list_item, new String[] { "line1",
+                "line2" }, new int[] { android.R.id.text1, android.R.id.text2 });
         setListAdapter(listAdapter);
     }
-    
+
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
-        
+
         Intent i = new Intent(this, FriendList.class);
         startActivity(i);
     }
-    
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return true;
     }
-    
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.options:
-                Intent i = new Intent(this, OptionsActivity.class);
-                startActivity(i);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        case R.id.options:
+            Intent i = new Intent(this, OptionsActivity.class);
+            startActivity(i);
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
         }
     }
 }
