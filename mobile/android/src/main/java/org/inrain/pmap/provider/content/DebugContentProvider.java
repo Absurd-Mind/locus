c65751f9a1266f9ebc56d16593792df1a705414c
Peter J. Kranz <peter@myref.net>
2012-12-12 04:51:00 +0100

    new debugging friend added

--- mobile/android/src/main/java/org/inrain/pmap/provider/content/DebugContentProvider.java
+++ mobile/android/src/main/java/org/inrain/pmap/provider/content/DebugContentProvider.java
@@ -11,16 +11,25 @@ public class DebugContentProvider implements ContentProvider {
     
     public List<Friend> getFriendList() {
         List<Friend> friends = new ArrayList<Friend>();
+        
         friends.add(new Friend("Fred"));
         Location loc = new Location("mock");
         loc.setLongitude(9.687778);
         loc.setLatitude(53.475833);
         friends.get(0).setLocation(loc);
+        
         friends.add(new Friend("Alfons"));
         Location loc2 = new Location("mock");
         loc2.setLongitude(9.685778);
         loc2.setLatitude(53.465833);
         friends.get(1).setLocation(loc2);
+        
+        friends.add(new Friend("Rewe"));
+        Location loc3 = new Location("mock");
+        loc3.setLongitude(11.001456);
+        loc3.setLatitude(49.575736);
+        friends.get(2).setLocation(loc3);
+        
         return friends;
     }
 }
