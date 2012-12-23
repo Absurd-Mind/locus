30f8e7e870d63c83cd3a9de19054bccf86dc90a6
Peter J. Kranz <peter@myref.net>
2012-12-23 22:12:28 +0100

    changed GET to POST

--- mobile/android/src/main/java/org/inrain/pmap/services/RealLocationSender.java
+++ mobile/android/src/main/java/org/inrain/pmap/services/RealLocationSender.java
@@ -1,14 +1,20 @@
 package org.inrain.pmap.services;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.inrain.pmap.Constants;
 import org.inrain.pmap.Util;
 import org.inrain.pmap.provider.preferences.PreferencesProvider;
@@ -39,7 +45,7 @@ public class RealLocationSender implements LocationSender {
     }
 
     private boolean sendLocationToServer(Location location) {
-        HttpGet request = new HttpGet();
+        HttpPost request = new HttpPost();
         boolean prepareRequestSucceeded = prepareRequest(request, location);
         if (!prepareRequestSucceeded) {
             return false;
@@ -57,30 +63,47 @@ public class RealLocationSender implements LocationSender {
         return true;
     }
 
-    private boolean prepareRequest(HttpGet request, Location location) {
+    private boolean prepareRequest(HttpPost request, Location location) {
         URI uri = null;
         try {
-            uri = createServerURI(location);
+            uri = createServerURI();
         } catch (URISyntaxException e) {
+            return false;
+        }
+        
+        String host = uri.getHost();
+        if (host == null) {
+            logger.trace("host is invalid");
             return false;
         }
 
         logger.trace("resulting URI: {}", uri.toString());
         request.setURI(uri);
+        
+        String username = preferencesProvider.getUsername();
+        
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+        nameValuePairs.add(new BasicNameValuePair("username", username));
+        nameValuePairs.add(new BasicNameValuePair("latitude", Double.toString(location.getLatitude())));
+        nameValuePairs.add(new BasicNameValuePair("longitude", Double.toString(location.getLongitude())));
+        nameValuePairs.add(new BasicNameValuePair("accuracy", Float.toString(location.getAccuracy())));
+        nameValuePairs.add(new BasicNameValuePair("provider", location.getProvider()));
+        
+        try {
+            request.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+        } catch (UnsupportedEncodingException e) {
+            return false;
+        }
+        
         return true;
     }
 
-    private URI createServerURI(Location location) throws URISyntaxException {
-        String username = preferencesProvider.getUsername();
+    private URI createServerURI() throws URISyntaxException {
         String serverUrl = preferencesProvider.getServerURL();
 
         String url = String.format(Locale.ENGLISH,
                                    Constants.SERVER_UPDATE_FORMAT_STRING,
-                                   serverUrl,
-                                   username,
-                                   location.getLatitude(),
-                                   location.getLongitude(),
-                                   location.getAccuracy());
+                                   serverUrl);
         URI uri = new URI(url);
         return uri;
     }
